cf78cbcc46ba582a312d7c149481ea9284656c4d
Max Kostyuk <kostyuk.max@gmail.com>
2022-12-25 20:33:36 +0600

    feat: в класс User добавлено поле friendsList

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -6,8 +6,11 @@ import ru.yandex.practicum.filmorate.validation.ContainsNoSpaces;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class User {
@@ -22,12 +25,15 @@ public class User {
     private String name;
     @Past
     private LocalDate birthday;
+    @NotNull
+    private Set<Integer> friendsList;
 
-    public User(int id, String email, String login, LocalDate birthday) {
+    public User(int id, String email, String login, LocalDate birthday, Set<Integer> friendsList) {
         this.id = id;
         this.email = email;
         this.login = login;
         this.name = login;
         this.birthday = birthday;
+        this.friendsList = friendsList;
     }
 }
