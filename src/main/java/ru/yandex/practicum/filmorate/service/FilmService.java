75a946c8f7ba5043748aee87fca6539c916d676a
Max Kostyuk <kostyuk.max@gmail.com>
2022-12-25 23:26:30 +0600

    feat: реализован класс FilmService с бизнес логикой

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -0,0 +1,48 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class FilmService {
+
+    private final FilmStorage filmStorage;
+
+    public Collection<Film> getAllFilms() {
+        return filmStorage.getAll();
+    }
+
+    public Film createFilm(Film film) {
+        return filmStorage.create(film);
+    }
+
+    public Film updateFilm(Film film) {
+        return filmStorage.update(film);
+    }
+
+    public void addLikeToFilm(int id, int userId) {
+        Film film = filmStorage.getById(id);
+        film.getLikesFromUsers().add(userId);
+        filmStorage.update(film);
+    }
+
+    public void deleteLikeOfFilm(int id, int userId) {
+        Film film = filmStorage.getById(id);
+        film.getLikesFromUsers().remove(userId);
+        filmStorage.update(film);
+    }
+
+    public Collection<Film> getMostPopular(int size) {
+        return filmStorage.getAll()
+                .stream()
+                .sorted((f0,f1) -> f0.getLikesFromUsers().size() - f1.getLikesFromUsers().size())
+                .limit(size)
+                .collect(Collectors.toList());
+    }
+}
