64a69cc016ba073344e41865328471aede581fc0
Max Kostyuk <kostyuk.max@gmail.com>
2022-12-25 23:06:53 +0600

    feat: в класс Film добавлено поле likesFromUsers

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,9 +6,11 @@ import lombok.Data;
 import ru.yandex.practicum.filmorate.validation.ReleaseDateValidation;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -23,5 +25,7 @@ public class Film {
     private LocalDate releaseDate;
     @Positive
     private int duration;
+    @NotNull
+    private Set<Integer> likesFromUsers;
 
 }
