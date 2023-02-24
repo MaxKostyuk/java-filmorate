7b895b510114193c04d31035fcb77973ebeae659
Stern_Ritter <fadeef.k@yandex.ru>
2023-02-24 23:00:47 +0300

    Refactoring create and update director service methods

--- src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
@@ -23,14 +23,12 @@ public class DirectorService {
 
     public Director update(Director director) {
         int id = director.getId();
-        try {
-            storage.get(id);
-            storage.update(director);
-            String name = director.getName();
-            return Director.builder().id(id).name(name).build();
-        } catch (IncorrectResultSizeDataAccessException ex) {
+        int updatedRowsCount = storage.update(director);
+        if(updatedRowsCount == 0) {
             throw new ElementNotFoundException(String.format(DIRECTOR_NOT_EXISTS_TEMPLATE, id), director);
         }
+        String name = director.getName();
+        return Director.builder().id(id).name(name).build();
     }
 
     public Director get(int id) {
